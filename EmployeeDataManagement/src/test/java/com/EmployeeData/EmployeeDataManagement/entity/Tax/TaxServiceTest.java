68e75764519b7daf701e508acbe7427277824b54
Phani Krishna <57516776+phanikrishna819@users.noreply.github.com>
2022-12-20 17:15:09 +0530

    Update TaxServiceTest.java

--- EmployeeDataManagement/src/test/java/com/EmployeeData/EmployeeDataManagement/entity/Tax/TaxServiceTest.java
+++ EmployeeDataManagement/src/test/java/com/EmployeeData/EmployeeDataManagement/entity/Tax/TaxServiceTest.java
@@ -32,23 +32,17 @@ class TaxServiceTest {
     @InjectMocks
     private TaxService taxService;
 
-    Employee employee;
 
-//    @Before
-//    public void setUp()
-//    {
-//
-//    }
 
 
     @Test
     void getSingleTax() throws ChangeSetPersister.NotFoundException {
 
         //Preparation/Stub:
-        Mockito.when(employeeRepository.findById("1")).thenReturn(Optional.of(new Employee("Phani","Krishna","9515757750","phanikrishna819@gmail.com","24Nov",1,40000)));
+        Mockito.when(employeeRepository.findById("1")).thenReturn(Optional.of(new Employee("Phani","Krishna","957778885544","phanikrishna9@gmail.com","24Nov",1,40000)));
         //Action:
         List<Tax> tax = taxService.getSingleTax(1);
         //Assertion
-        assertEquals(tax.get(0).getTaxDeduction(),tax.get(0).getTaxDeduction());
+        assertEquals(480000,tax.get(0).getAnnual_salary());
     }
 }
